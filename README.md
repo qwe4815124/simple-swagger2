f5cadbda07b9ba7f9a51c6a3c53058482bdee397
zhengjing <zhengjing@58nfbst.com>
2017-11-24 11:23:18 +0800

    通用swagger

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # simple-swagger2
 通过配置的方式来玩swagger2
+
+使用方式 
+公用的启用swagger代码 修改配置就可以
+
+```yaml
+swagger:
+  enable: true
+  o-auth2:
+    enable: true
+    app-key: basic-api
+    app-name: 基础api
+    authorization-url: http://39.108.51.1/se/oauth/authorize
+  apis:
+    user:
+      packageName: com.jing21.learn.basic.api.user.controller
+      author:
+        name: dev1
+    system:
+      packageName: com.jing21.learn.basic.api.controller
+      author:
+        name: dev2
+      title: dev2 API
+      description: 接口文档
+    
+```  
+ 
+具体的配置查看`SwaggerProperties`
---
+++ src/main/java/com/jing21/simpleswagger2/SwaggerProperties.java
@@ -0,0 +1,113 @@
+package com.jing21.simpleswagger2;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+/**
+ * Create By zhengjing on 2017/11/13 11:45
+ */
+@Configuration
+@ConfigurationProperties("swagger")
+@Data
+public class SwaggerProperties {
+
+    private Boolean enable;
+    /**
+     * api配置
+     */
+    private Map<String, ApiInfoProperties> apis;
+
+    @NestedConfigurationProperty
+    private OAuth2Properties oAuth2 = new OAuth2Properties();
+
+    @Data
+    public static class OAuth2Properties {
+
+        private boolean enable = false;
+
+        private String type = "oauth2";
+
+        private String authorizationUrl = "http://localhost:8830/se/oauth/authorize";
+
+        private String authClientId = "swagger";
+
+        private String authClientSecret = "swagger";
+
+        /**
+         * 授权信息
+         */
+        private String scopeCode = "openid";
+        /**
+         * 描述
+         */
+        private String scopeDesc = "用户信息";
+
+        /**
+         * 应用id
+         */
+        private String appKey;
+
+        /**
+         * 应用名称
+         */
+        private String appName;
+    }
+
+    @Data
+    public static class ApiInfoProperties {
+
+        /**
+         * 作者
+         */
+        private Author author;
+
+        /**
+         * 分组名
+         */
+        private String group;
+
+        /**
+         * 包名
+         */
+        private String packageName;
+
+        /**
+         * 描述
+         */
+        private String description = "swagger api documentation";
+
+        /**
+         * 标题
+         **/
+        private String title = "Api Documentation";
+
+        /**
+         * 版本
+         **/
+        private String version = "snapshoot";
+        /**
+         * 许可证
+         **/
+        private String license = "Apache 2.0";
+        /**
+         * 许可证URL
+         **/
+        private String licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0";
+        /**
+         * 服务条款URL
+         **/
+        private String termsOfServiceUrl = "urn:tos";
+
+    }
+
+    @Data
+    public static class Author {
+        private String name;
+        private String url;
+        private String email;
+    }
+}
